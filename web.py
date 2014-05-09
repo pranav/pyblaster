fd13d5adcd1a26f22adb6a68425556e7dddf189c
Pranav Gandhi <pranavrulz@gmail.com>
2014-05-09 01:07:43 +0000

    Added web api endpoint and requirements.pip

---
+++ web.py
@@ -0,0 +1,23 @@
+
+import pyblaster
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+
+@app.route("/set", methods=['GET'])
+def set():
+    color_vals = []
+    for k in ['r', 'b', 'g']:
+        color_vals.append(request.args.get(k))
+
+    pyblaster.set(pyblaster.RED_GPIO, color_vals[0])
+    pyblaster.set(pyblaster.BLUE_GPIO, color_vals[1])
+    pyblaster.set(pyblaster.GREEN_GPIO, color_vals[2])
+
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
