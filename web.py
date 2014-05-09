65ef6fd94f5332c995e1fe0cd3bfee5a042bdc66
Pranav Gandhi <pgandhi@hubspot.com>
2014-05-09 18:29:11 -0400

    Added better color picker and support hex value

--- web.py
+++ web.py
@@ -1,19 +1,28 @@
-
+import struct
 import pyblaster
-from flask import Flask
+import re
+from flask import Flask, redirect, url_for
 from flask import request
 app = Flask(__name__, static_url_path='/static')
 
 
+@app.route("/")
+def index():
+    return redirect(url_for('static', filename='index.html'))
+
 @app.route("/set", methods=['GET'])
 def set():
-    color_vals = []
-    for k in ['r', 'b', 'g']:
-        color_vals.append(request.args.get(k))
+    if request.args.get('hex'):
+        color_vals = [1-(float(int(i, 16))/255) for i in re.findall('..?', request.args.get('hex'))]
+    else:
+        color_vals = [1-(float(request.args.get(k)/255)) for k in ['r', 'b', 'g']]
+
 
     pyblaster.set(pyblaster.RED_GPIO, color_vals[0])
-    pyblaster.set(pyblaster.BLUE_GPIO, color_vals[1])
-    pyblaster.set(pyblaster.GREEN_GPIO, color_vals[2])
+    pyblaster.set(pyblaster.GREEN_GPIO, color_vals[1])
+    pyblaster.set(pyblaster.BLUE_GPIO, color_vals[2])
+
+    print color_vals
 
     return 'OK'
 
